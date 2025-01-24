a578b82a6140a479e432baeb66a7ff69ab4dbd1c
Nikhilkumar Varma <nikhil.v@nimapinfotech.com>
2025-01-24 16:45:44 +0530

    LINQ Extension Methods

---
+++ LINQDemo/BookRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQDemo
+{
+    internal class BookRepository
+    {
+        public IEnumerable<Book> GetBooks()
+        {
+            return new List<Book>
+            {
+                new Book(){Title = "ADO.Net Step by Step", Price = 5},
+                new Book(){Title = "ASP.Net MVC", Price = 9.99f},
+                new Book(){Title = "ASP.Net Web API", Price = 12},
+                new Book(){Title = "C# Advanced Topics", Price = 7},
+                new Book(){Title = "C# Advanced Topics", Price = 9}
+            };
+        }
+    }
+}
---
+++ LINQDemo/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace LINQDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Trying some LINQ Extension Methods:
+            var books = new BookRepository().GetBooks();
+            var bookData = books.FirstOrDefault(b => b.Title == "C# Advanced Topics");
+            Console.WriteLine(bookData.Title+" "+bookData.Price);
+            //There are some other Extensions try it later
+            //This are used for pagination.
+            //It skips perticular number of data and take data:
+            Console.WriteLine("Following data has been used for pagination:");
+            var pagedBooks = books.Skip(2).Take(3);
+            foreach (var pageBook in pagedBooks)
+            {
+                Console.WriteLine(pageBook.Title+" "+pageBook.Price);
+            }
+            //Let's find maximum price:
+            var maxPricedBook = books.FirstOrDefault(b => b.Price == books.Max(b => b.Price));
+            Console.WriteLine("Highly priced book is "+maxPricedBook.Title);
+            //Count()
+            var TotalBooks = books.Count();
+            Console.WriteLine("Total books in shop are "+TotalBooks);
+            //Sum()
+            var TotalPrice = books.Sum(b => b.Price);
+            Console.WriteLine("Total price of all books is "+TotalPrice);
+            //Average()
+            var avg = books.Average(b => b.Price);
+            Console.WriteLine("Average of book's price is "+avg);
+
+            ////LINQ Query Operators:
+            //var books = new BookRepository().GetBooks();
+            //var cheapBooks = from b in books
+            //                 where b.Price < 10
+            //                 orderby b.Title
+            //                 select b;
+            //foreach (var book in cheapBooks)
+            //{
+            //    Console.WriteLine(book.Title + " " + book.Price);
+            //}
+
+            ////LINQ Extention Method:
+            ////Here Select query is powerful we get data and pass 
+            ////only single column and their datatype will be there
+            ////Otherwise it will be object.
+            //var books = new BookRepository().GetBooks();
+            //var cheapBoooks = books.Where(b => b.Price < 10)
+            //    .OrderBy(b => b.Title)
+            //    .Select(b => b.Title);
+            //foreach (var book in cheapBoooks);
+            //{
+            //    Console.WriteLine(book);
+            //}
+
+            ////Normal way of sorting data
+            //var books = new BookRepository().GetBooks();
+            //var cheapBooks = new List<Book>();
+            //foreach (var book in books)
+            //{
+            //    if(book.Price < 10)
+            //    {
+            //        cheapBooks.Add(book);
+            //    }
+            //}
+            //foreach (var b in cheapBooks)
+            //{
+            //    Console.WriteLine(b.Title + " " + b.Price);
+            //}
+        }
+    }
+}
